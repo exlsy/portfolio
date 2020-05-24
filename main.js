0164fbf321b57904581b2d3c3036049bf41d7ce9
exlsy <exsylee@gmail.com>
2020-05-24 13:14:03 +0900

    JS CSS: bug fix

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ document.addEventListener('scroll', () => {
 
 
 // Handle scrolling when tapping on the navbar menu
-const navbarMenu = document.querySelector('.navbar__menu');
+const navbarMenu = document.querySelector('.navbar__menu ul');
 navbarMenu.addEventListener('click', (event) => {
     const target = event.target;
     const link = target.dataset.link;
